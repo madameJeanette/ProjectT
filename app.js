0756da6cef988a652a4b8dd1addf946c30bef73d
Danielle Meerburg <danielle_meerburg@live.nl>
2018-12-02 20:18:47 +0100

    save the tarantulas!

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/tarantulaAPI');
 
 var Tarantula = require('./models/tarantulaModel');
 
 var app = express();
+
 var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 var ttRouter = express.Router();
 
 ttRouter.route('/Tarantulas')
+ .post(function (req,res) {
+   var tarantula = new Tarantula(req.body);
+
+   tarantula.save();
+   res.status(201).send(tarantula);
+     
+ })
  .get(function (req,res) {
     Tarantula.find(function(err,tarantulas){
         if(err)
-          console.log(err)
+          res.status(500).send(err);
         else 
          res.json(tarantulas);
     }); 
--- models/tarantulaModel.js
+++ models/tarantulaModel.js
@@ -1,12 +1,11 @@
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema();
+    Schema = mongoose.Schema;
 
 var tarantulaModel = new Schema({
    name: {type: String},
-   'Latin name':{type: String},
-   Habitat:{type: String},
-   'In collection': {type: Boolean, default:false}
-
+   latinName: {type: String},
+   habitat:{type: String},
+   collected: {type: Boolean, default:false}
 });
 
 module.exports= mongoose.model('Tarantula', tarantulaModel);
