d6d316c8a6623b6290e69b64fabc791c793e65b6
Danielle Meerburg <danielle_meerburg@live.nl>
2018-12-02 15:23:35 +0100

    dependencies

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-}
+var express = required('express');
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+var app = express();
+var port = process.env.PORT || 3000;
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
-
-// Database Name
-const dbName = 'myproject';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  const db = client.db(dbName);
-
-  insertDocuments(db, function() {
-    client.close();
-  });
+app.get('/', function(req, res){
+  req.send('welcome to my API');
 });
 
-
-
+app.listen(port, function(){
+ console.log('Running on PORT: ' + port);
+});
