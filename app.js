8cb451faf061819bfe0cc3e6150f9a94c552e3b0
Danielle Meerburg <danielle_meerburg@live.nl>
2018-12-02 17:22:03 +0100

    Get Tarantulas :)

--- app.js
+++ app.js
@@ -3,6 +3,18 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var ttRouter = express.Router();
+
+ttRouter.route('/Tarantulas')
+ .get(function (req,res) {
+    var responseJson = {hello: "This is my API"}; 
+
+    res.json(responseJson);
+ });
+
+app.use('/api', ttRouter);
+
+
 app.get('/', function(req, res){
   res.send('welcome to my API');
 });
