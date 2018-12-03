dfb45905b90acfe5d5f6478b3625e12e3ee6fd78
Danielle Meerburg <danielle_meerburg@live.nl>
2018-12-03 14:14:06 +0100

    added required fields

--- models/tarantulaModel.js
+++ models/tarantulaModel.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var tarantulaModel = new Schema({
-   name: {type: String},
-   latinName: {type: String},
-   habitat:{type: String},
+   name: {type: String, required: true},
+   latinName: {type: String, required: true},
+   habitat:{type: String, required: true},
    collected: {type: Boolean, default:false}
 });
 
